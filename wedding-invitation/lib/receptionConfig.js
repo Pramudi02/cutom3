5ddb9d5c9914a6d8df54a701c0564c647e310180
Pramudi Samarawickrama <samarawickramappn.22@uom.lk>
2026-08-17 21:50:48 +0530

    Update RSVP deadline to August 22, 2026

--- wedding-invitation/lib/receptionConfig.js
+++ wedding-invitation/lib/receptionConfig.js
@@ -62,7 +62,7 @@ export const receptionConfig = {
   },
   rsvp: {
     whatsappNumber: '766155777',
-    deadline: 'August 16, 2026',
+    deadline: 'August 22, 2026',
     maxGuests: 2,
     dietaryTitle: null,
     dietaryItems: null,
